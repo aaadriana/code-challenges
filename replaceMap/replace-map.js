331e6cd62b56013fedd4a9665d55509f69a5094d
Adriana Stanciu <82960475+aaadriana@users.noreply.github.com>
2021-07-29 13:33:58 +0100

    Update replace-map.js

--- replaceMap/replace-map.js
+++ replaceMap/replace-map.js
@@ -1,7 +1,12 @@
 /*
-Imagine that the array method 'map' has vanished. It's no longer available and therefore you can't use it anymore. Your initial goal in this challenge is to implement a replacement for 'map' - 'yourMapReplacement'. It needs to function exactly like map.
+Imagine that the array method 'map' has vanished.
+It's no longer available and therefore you can't use it anymore. 
+Your initial goal in this challenge is to implement a replacement for 'map' - 'yourMapReplacement'. 
+It needs to function exactly like map.
 
-Your main goal is to pass a function called 'roundToEven' to your replacement map function. 'roundToEven' takes arrays of numbers and rounds them to the next even number. Even numbers inside the array will not be modified.
+Your main goal is to pass a function called 'roundToEven' to your replacement map function. 
+'roundToEven' takes arrays of numbers and rounds them to the next even number. 
+Even numbers inside the array will not be modified.
 
 */
 
